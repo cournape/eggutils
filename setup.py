cf7aeaef6d4d3535b7fae307a5b0048a8feabfe0
David Cournapeau <cournape@gmail.com>
2009-10-24 15:19:06 +0900

    REL: release version 0.0.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 if __name__ == "__main__":
     setup(name="eggutils",
-        version="0.0.1",
+        version="0.0.2",
         license="BSD",
         description="A set of utilities to create/manipulate eggs",
         long_description=open("README").read(),
